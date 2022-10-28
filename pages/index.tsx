680ff6f1141a0e7be2348a0d48d9714b176822c2
joska potin <jpotin@gmail.com>
2022-10-29 01:47:02 +0200

    add quotes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from "next/head"
 import Carousel from "../components/carousel/carousel"
+import Quote from "../components/quote/quote"
 import { PATH, SHEET } from "../constants/constants"
 import type { PaintingInterface } from "../services/painting"
 import { getPaintings } from "../services/painting"
@@ -28,29 +29,31 @@ export default function Home({ paintings }: Props) {
       <Carousel items={carouselItems} />
 
       <section>
-        <p>
-          Dans des huiles posées avec précision sur un décor soigné, support en
-          bois ou portail en biseau, il décline des ostéomorphes placés plus
-          souvent verticalement sur fond uni noir de préférence, fichés sur un
-          tube d&apos;acier ou articulés les uns aux autres, formant un monde
-          sculptural étrange.
-        </p>
-        <p>
-          De temps à autre une allusion au vivant comme cette construction dans
-          laquelle se détache un bec d&apos;oiseau.
-        </p>
-        <p>
-          Si Hellée, (1999) représente cette peinture dans laquelle la sculpture
-          est implicite, il annonce une explicite sculpture de bois, peint,
-          Hellée, (2009).
-        </p>
-        <p>
-          Un portique laisse pendre, ossement acérés, des cadres et des boules
-          dans une allégorie létale de tarlatane colorée. 2007
-        </p>
-        <cite className="mt-10 block">
-          <p className="text-end">Le Delarge</p>
-        </cite>
+        <Quote>
+          <p>
+            Dans des huiles posées avec précision sur un décor soigné, support
+            en bois ou portail en biseau, il décline des ostéomorphes placés
+            plus souvent verticalement sur fond uni noir de préférence, fichés
+            sur un tube d&apos;acier ou articulés les uns aux autres, formant un
+            monde sculptural étrange.
+          </p>
+          <p>
+            De temps à autre une allusion au vivant comme cette construction
+            dans laquelle se détache un bec d&apos;oiseau.
+          </p>
+          <p>
+            Si Hellée, (1999) représente cette peinture dans laquelle la
+            sculpture est implicite, il annonce une explicite sculpture de bois,
+            peint, Hellée, (2009).
+          </p>
+          <p>
+            Un portique laisse pendre, ossement acérés, des cadres et des boules
+            dans une allégorie létale de tarlatane colorée. 2007
+          </p>
+          <cite className="mt-10 block">
+            <p className="text-end">Le Delarge</p>
+          </cite>
+        </Quote>
       </section>
     </>
   )
