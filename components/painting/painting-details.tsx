b309a145a09f1d1c008cf3234cc9b2c0949aaebf
joska potin <jpotin@gmail.com>
2022-10-29 17:55:22 +0200

    move painting details

--- components/painting/painting-details.tsx
+++ components/painting/painting-details.tsx
@@ -1,4 +1,40 @@
-function PaintingDetails() {
-  return <div>PaintingDetails</div>
+import Image from "next/image"
+import { PATH } from "../../constants/constants"
+import { IPainting } from "../../services/painting"
+
+type Props = {
+  painting: IPainting
+}
+
+function PaintingDetails({ painting }: Props) {
+  const { title, year, height, width, filename, imageWidth, imageHeight } = painting
+  return (
+    <>
+      <Image
+        className="md:justify-self-end"
+        alt={title}
+        src={`${PATH.PAINTINGS.SOURCE}/${filename}`}
+        width={imageWidth}
+        height={imageHeight}
+      />
+
+      <ul className="self-center">
+        <li className="mb-2 text-2xl">
+          <span className="mb-4 tracking-widest text-amber-900">Titre:</span> {title}
+        </li>
+        <li>
+          <time dateTime={year}>
+            <span className="mb-4 tracking-widest text-amber-900">Année:</span> {year}
+          </time>
+        </li>
+        <li>
+          <span className="mb-4 tracking-widest text-amber-900">Hauteur:</span> {height} cm
+        </li>
+        <li>
+          <span className="mb-4 tracking-widest text-amber-900">Largeur:</span> {width} cm
+        </li>
+      </ul>
+    </>
+  )
 }
 export default PaintingDetails
--- pages/paintings/[slug]/index.tsx
+++ pages/paintings/[slug]/index.tsx
@@ -1,8 +1,7 @@
-import Image from "next/image"
 import type { GetStaticPaths, GetStaticProps, InferGetStaticPropsType } from "next/types"
 import { ParsedUrlQuery } from "querystring"
+import PaintingDetails from "../../../components/painting/painting-details"
 import PaintingListItem from "../../../components/painting/painting-list-item"
-import { PATH } from "../../../constants/constants"
 import { getPaintings } from "../../../services/painting"
 
 interface IParams extends ParsedUrlQuery {
@@ -14,34 +13,9 @@ function Painting({
   currentPainting,
   nextPainting,
 }: InferGetStaticPropsType<typeof getStaticProps>) {
-  const { title, year, height, width, filename, imageWidth, imageHeight } = currentPainting
-
   return (
     <>
-      <Image
-        className="md:justify-self-end"
-        alt={title}
-        src={`${PATH.PAINTINGS.SOURCE}/${filename}`}
-        width={imageWidth}
-        height={imageHeight}
-      />
-
-      <ul className="self-center">
-        <li className="mb-2 text-2xl">
-          <span className="mb-4 tracking-widest text-amber-900">Titre:</span> {title}
-        </li>
-        <li>
-          <time dateTime={year}>
-            <span className="mb-4 tracking-widest text-amber-900">Année:</span> {year}
-          </time>
-        </li>
-        <li>
-          <span className="mb-4 tracking-widest text-amber-900">Hauteur:</span> {height} cm
-        </li>
-        <li>
-          <span className="mb-4 tracking-widest text-amber-900">Largeur:</span> {width} cm
-        </li>
-      </ul>
+      <PaintingDetails painting={currentPainting} />
 
       <nav className="relative col-span-full flex w-full justify-evenly gap-2">
         <PaintingListItem painting={prevPainting} />
