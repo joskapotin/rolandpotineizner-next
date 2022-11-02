32b8ecc22ba8789900c4d2ddb81bc8fdf9dd48ab
joska potin <jpotin@gmail.com>
2022-11-02 17:22:07 +0100

    remove label in front of the title

--- components/painting/painting-details.tsx
+++ components/painting/painting-details.tsx
@@ -20,22 +20,22 @@ function PaintingDetails({ painting }: Props) {
         priority
       />
 
-      <ul className="self-center text-center lg:text-start">
-        <li className="mb-2 text-2xl">
-          <span className="mb-4 tracking-widest text-amber-900">Titre:</span> {title}
-        </li>
-        <li>
-          <time dateTime={year}>
-            <span className="mb-4 tracking-widest text-amber-900">Année:</span> {year}
-          </time>
-        </li>
-        <li>
-          <span className="mb-4 tracking-widest text-amber-900">Hauteur:</span> {height} cm
-        </li>
-        <li>
-          <span className="mb-4 tracking-widest text-amber-900">Largeur:</span> {width} cm
-        </li>
-      </ul>
+      <div className="flex flex-col items-center self-center text-center lg:text-start">
+        <h1 className="mb-8 text-4xl">{title}</h1>
+        <ul>
+          <li>
+            <time dateTime={year}>
+              <span className="mb-4 tracking-widest text-amber-900">Année:</span> {year}
+            </time>
+          </li>
+          <li>
+            <span className="mb-4 tracking-widest text-amber-900">Hauteur:</span> {height} cm
+          </li>
+          <li>
+            <span className="mb-4 tracking-widest text-amber-900">Largeur:</span> {width} cm
+          </li>
+        </ul>
+      </div>
     </>
   )
 }
