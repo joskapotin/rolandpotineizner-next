1979d7267f02c3257a06a8d8bd01570f177aac7d
joska potin <jpotin@gmail.com>
2022-11-25 21:19:46 +0100

    move framermotion into his own component

--- components/layout/root-layout.tsx
+++ components/layout/root-layout.tsx
@@ -1,25 +1,15 @@
-import { AnimatePresence, motion } from "framer-motion"
 import Head from "next/head"
-import { useRouter } from "next/router"
-import type { ReactNode } from "react"
 import photoRoland from "../../assets/images/roland01.jpg"
+import PageTransition from "../motion/page-transition"
 import SkipToMainContent from "../skip-to-main-content/skip-to-main-content"
 import Footer from "./footer/footer"
 import Header from "./header/header"
 
 type props = {
-  children: ReactNode
-}
-
-const variants = {
-  hidden: { opacity: 0 },
-  enter: { opacity: 1 },
-  exit: { opacity: 0 },
+  children: React.ReactNode
 }
 
 function RootLayout({ children }: props) {
-  const { asPath } = useRouter()
-
   return (
     <>
       <Head>
@@ -53,20 +43,7 @@ function RootLayout({ children }: props) {
 
       <Header />
 
-      <AnimatePresence initial={false} mode="wait">
-        <motion.main
-          key={asPath}
-          variants={variants} // Pass the variant object into Framer Motion
-          initial="hidden" // Set the initial state to variants.hidden
-          animate="enter" // Animated state to variants.enter
-          exit="exit" // Exit state (used later) to variants.exit
-          transition={{ type: "spring", duration: 0.3 }} // Set the transition to linear
-          id="main-content"
-          className="container mx-auto grid flex-grow origin-top content-start justify-items-center gap-y-24 px-2 sm:px-0 md:grid-cols-2 md:gap-x-6 lg:gap-x-7 xl:gap-x-8 2xl:gap-x-9"
-        >
-          {children}
-        </motion.main>
-      </AnimatePresence>
+      <PageTransition>{children}</PageTransition>
 
       <Footer />
     </>
---
+++ components/motion/page-transition.tsx
@@ -0,0 +1,35 @@
+import { AnimatePresence, motion } from "framer-motion"
+import { useRouter } from "next/router"
+
+type props = {
+  children: React.ReactNode
+}
+
+const variants = {
+  hidden: { opacity: 0 },
+  enter: { opacity: 1 },
+  exit: { opacity: 0 },
+}
+
+function PageTransition({ children }: props) {
+  const { asPath } = useRouter()
+
+  return (
+    <AnimatePresence initial={false} mode="wait">
+      <motion.main
+        key={asPath}
+        variants={variants} // Pass the variant object into Framer Motion
+        initial="hidden" // Set the initial state to variants.hidden
+        animate="enter" // Animated state to variants.enter
+        exit="exit" // Exit state (used later) to variants.exit
+        transition={{ type: "spring", duration: 0.3 }} // Set the transition to linear
+        id="main-content"
+        className="container mx-auto grid flex-grow origin-top content-start justify-items-center gap-y-24 px-2 sm:px-0 md:grid-cols-2 md:gap-x-6 lg:gap-x-7 xl:gap-x-8 2xl:gap-x-9"
+      >
+        {children}
+      </motion.main>
+    </AnimatePresence>
+  )
+}
+
+export default PageTransition
